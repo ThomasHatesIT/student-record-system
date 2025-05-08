375f9956cacb11188904c4de765173c478b9e960
ThomasHatesIT <thomasbernardo910@gmail.com>
2025-05-08 10:05:12 +0800

    done with post form and delete function

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -8,7 +8,7 @@ class CoursesController extends Controller
     public function index(Request $request)
     {
 
-        $courses = Course::all();
+        $courses = Course::latest()->get();
         
 
         return view('courses.index', [
@@ -29,4 +29,47 @@ class CoursesController extends Controller
         ]);
     }
 
+    public function store(request $request){
+        $request->validate([
+          'name' => ['required', 'min:3'],
+            'description' => ['required', 'min:1'],
+        ]);
+
+        Course::create([
+            'name' => $request->name,    
+            'description' => $request->description,
+        ]);
+        return redirect('/courses');
+    }
+
+    public function edit(){
+
+    }
+    public function update(){
+        $request->validate([
+            'name' => ['required', 'min:3'],
+            'description' => ['required', 'min:1'],
+            
+        ]);
+
+        $course = Courese::findOrFail($id);
+
+        $course->update([
+            'name' => $request->name,    
+            'description' => $request->description,
+            
+        ]);
+
+        return redirect('/courses/' . $course->id)
+            ->with('success', 'Student updated successfully.');
+    }
+
+
+    public function destroy($id)
+    {
+        $course = Course::findOrFail($id);
+        $course->delete();
+
+        return redirect('/courses');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Models\Course;
 
 Route::get('/', [StudentController::class, 'index']);
 Route::get('/students/create', [StudentController::class, 'create']);
-Route::post('/', [StudentController::class, 'store']);
+Route::post('/', [StudentController::class, 'store']);  
 Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
 Route::patch('/students/{id}', [StudentController::class, 'update']);
@@ -20,4 +20,7 @@ Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 
 Route::get('/courses', [CoursesController::class, 'index']);
 Route::get('/courses/create', [CoursesController::class, 'create']);
+Route::post('/courses',[CoursesController::class,'store']);
 Route::get('courses/{id}', [CoursesController::class, 'show']);
+Route::get('courses/{id}/edit', [CoursesController::class, 'edit']);
+Route::delete('/courses/{id}', [CoursesController::class, 'destroy']);
